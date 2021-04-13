d4d3c53a1207578f7ffbd6b882c9dfb75611aa9a
Avinashmuthukumar <60893720+Avinashmuthukumar@users.noreply.github.com>
2021-04-13 11:38:54 +0400

    Update aws-iot-configuration.js

--- src/aws-iot-configuration.js
+++ src/aws-iot-configuration.js
@@ -1,7 +1,7 @@
 var awsIotConfiguration = {
-    endpoint: 'wss://a2mvse6841elo7-ats.iot.us-west-2.amazonaws.com/mqtt', 
+    endpoint: 'wss://a5okrjfeg54i5-ats.iot.us-west-2.amazonaws.com/mqtt', 
     region: 'us-west-2',
     poolId: 'us-west-2:d8b273d6-8d18-4fe7-81df-7d2ddd77587a',
-    host: 'a2mvse6841elo7-ats.iot.us-west-2.amazonaws.com'
+    host: 'a5okrjfeg54i5-ats.iot.us-west-2.amazonaws.com'
   };
  module.exports = awsIotConfiguration;
